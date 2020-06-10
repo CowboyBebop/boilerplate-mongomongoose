2d4da6be6ac3879891f0b58fdac600ebcaef571a
RebornInferno <antonpirogov@mail.ru>
2020-06-10 20:29:38 +0300

    fixed?

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://Bebop:BDSpikeSpiegel@codecluster-6ufys.mongodb.net/CodeCluster?retryWrites=true&w=majority"), { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 /** # SCHEMAS and MODELS #
