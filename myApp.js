3a0212ec67d38f336235dfaf9baae1c699540f86
RebornInferno <antonpirogov@mail.ru>
2020-06-10 20:05:49 +0300

    fixed?

--- myApp.js
+++ myApp.js
@@ -15,7 +15,7 @@
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
 
 /** # SCHEMAS and MODELS #
